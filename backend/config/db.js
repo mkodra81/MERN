bfbc17a256f62f722a20a3cd2f0ded022438fc31
Mateo <kodramateo4@gmail.com>
2025-03-31 22:14:39 +0200

    optimisation

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDb Connected: ${conn.connection.host}`);
+
+const conn = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  poolSize: 10
+});    console.log(`MongoDb Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`${error}`);
     process.exit(1);
